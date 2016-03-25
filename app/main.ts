2d2fcd557d8c1cc5905bde9f5260b8a25281e11b
ultrasonicsoft <uvbrainsolutions@outlook.com>
2016-03-25 18:48:11 +0530

    materialized css added

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,8 @@
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
+import {LocalStorageSubscriber} from 'angular2-localstorage/LocalStorageEmitter';
 
-bootstrap(AppComponent);
+var appPromise = bootstrap(AppComponent);
+
+// register LocalStorage, this registers our change-detection.
+// LocalStorageSubscriber(appPromise);
